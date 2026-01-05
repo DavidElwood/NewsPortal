1ddf8d7f77b78bb0e508e1621cda99bfff338149
Aerton-Chris Tasa <aerton-chris.tasa@ivkhk.ee>
2026-01-05 19:00:37 +0200

    Controller.php

---
+++ controller/Controller.php
@@ -0,0 +1,28 @@
+<?php
+
+class Controller {
+
+    public static function StartSite() {
+        $arr = News::getLast10News();
+        include_once 'view/start.php';
+    }
+
+    public static function AllCatergory() {
+        $arr = Category::getAllCategory();
+        include_once 'view/category.php';
+    }
+
+    public static function AllNews() {
+        $arr = News::getAllNews();
+        include_once 'view/allnews.php';
+    }
+
+    public static function NewsByID($id) {
+        $n = News::getNewsByID($id);
+        include_once 'view/readnews.php';
+    }
+    
+    public static function error404() {
+        include_once 'view/error404.php';
+    }
+}
