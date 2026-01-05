ca30a573265f1364c326206b3e7c5bb4a60ed1a2
Aerton-Chris Tasa <aerton-chris.tasa@ivkhk.ee>
2026-01-05 18:35:01 +0200

    layout.php

---
+++ view/layout.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> NEWSPORTAL </title>
+        <link rel="stylesheet" href="
+        https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity=
+        "sha384-ggOyR-iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous">
+        <link rel= "stylesheet" type ="text/css" href="style.css">
+        <link href="https://fonts.googleapis.com/css?family=Noto+Serif" rel="stylesheet">
+        <meta chatset="utf-8">
+    </head>
+    <body>
+        <nav class="one">
+        <ul class="topmenu">
+        <li><a href="#">Kategooriad<i class="fa fa-angle-down"></i></a>
+        <ul class="submenu">
+        <?php
+                Controller::AllCategory();
+        ?>
+        </ul>
+        </li>
+        <li><a href="testError">Info</a></li>
+        <li><a href="./">StardiLeht</a></li>
+
+        </ul>
+        </nav>
+        <section>
+            <div> class = 'divBox'>
+                <?php
+                if(isset($content)){
+                    echo $content;
+                }
+                else {
+                    echo '<h1>Content is Gone!</h1>';
+                }
+                ?>
+            </div>
+        </section>
+        <hr>
+        <p style="display:block; text-aling:center;">JKITp24 2026 a. &copy</p>
+    </body>
+
+</html>
