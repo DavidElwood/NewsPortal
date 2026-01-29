a939aeaf5825f1a9f88ee7f5029230b3fa54ebee
Aerton-Chris Tasa <aerton-chris.tasa@ivkhk.ee>
2026-01-29 18:02:29 +0200

    new news

---
+++ admin/routeAdmin/routingAdmin.php
@@ -0,0 +1,54 @@
+<?php
+$host = explode ('?', $_SERVER['REQUEST_URI']) [0];
+$num = substr_count($host, '/');
+$path = explode('/', $host) [$num];
+
+if ($path =='' OR $path == 'index.php' )
+{
+   $response = controllerAdmin::formLoginSite();
+}
+//--login
+elseif ($path == 'login')
+{
+   $response = controllerAdmin::loginAction();
+}
+//--logout
+elseif ($path == 'logout')
+{
+   $response = controllerAdmin::logoutAction();
+}
+//-listNews
+elseif($path=='newsAdmin'){
+   $response=controllerAdminNews::NewsList();
+}
+//-add news
+ elseif($path=='newsAdd'){
+   $response=controllerAdminNews::newsAddForm();
+ }
+
+ elseif($path =='newsAddResult'){
+   $response=controllerAdminNews::newsAddResult();
+ }
+
+//---edit
+elseif($path=='newsEdit' && isset($_GET['id'])) {
+$response=controllerAdminNews::newsEditForm($_GET['id']);
+}
+elseif($path == 'newsEditResult' && isset($_GET['id'])) {
+$response = controllerAdminNews::newsEditResult($_GET['id']);
+}
+
+
+elseif($path == 'newsDel' && isset($_GET['id'])) {
+    $response = controllerAdminNews::newsDeleteForm($_GET['id']);
+}
+   
+elseif($path == 'newsDelResult' && isset($_GET['id'])) {
+    $response = controllerAdminNews::newsDeleteResult($_GET['id']);
+}
+
+    
+else
+{
+    $response = controllerAdmin::error404();
+}
